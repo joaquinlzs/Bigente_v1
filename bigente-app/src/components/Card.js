25defb27fb20022e5cc41338a402d350a17be81d
joaquinlzs <lazojoaquin.a@gmail.com>
2023-09-16 15:52:46 -0300

    add spoiler button to text

--- bigente-app/src/components/Card.js
+++ bigente-app/src/components/Card.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CSS/Card.css';
 
 function Card({ title, subreddit, selftext, score, thumbnail, permalink }) {
+  const [isSpoilerVisible, setSpoilerVisibility] = useState(false);
+
+  const toggleSpoiler = () => {
+    setSpoilerVisibility(!isSpoilerVisible);
+  };
+  
   return (
     <div className="card">
       <a href={`https://www.reddit.com${permalink}`}>{title}</a>
-      <p>Subreddit: {subreddit}</p>
-      <p>{selftext}</p>
-      {thumbnail !== "self" ? <img src={thumbnail} alt="Imagen Reddit" /> : null}
+
+      {selftext && (
+        isSpoilerVisible ? (
+          <div>
+            <p>{selftext}</p>
+            <button className="buttontext" onClick={toggleSpoiler}>Ocultar texto</button>
+          </div>
+        ) : (
+          <div>
+            <button className="buttontext" onClick={toggleSpoiler}>Mostrar texto</button>
+          </div>
+        )
+      )}
+      
+      <div>
+        {thumbnail !== "self" ? <img src={thumbnail} alt="Imagen Reddit" /> : null}
+      </div>
+      
       <p>Score: {score}</p>
       
     </div>
