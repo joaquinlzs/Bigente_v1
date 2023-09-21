274a39d04e3fc759e54caa07f4f60831e9d3d568
joaquinlzs <lazojoaquin.a@gmail.com>
2023-09-21 08:34:18 -0300

    add fixed tags to cards with inline style

--- bigente-app/src/components/Card2.js
+++ bigente-app/src/components/Card2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CSS/Card2.css';
+import Tag from './Tag';
 
 // Importa las clases de FontAwesome para icono del enlace
 
@@ -7,17 +8,22 @@ function Card({ title, subreddit, selftext, score, thumbnail, permalink }) {
 
     return (
     <div className="card">
-        <h2 className="title">
-            {title}
-        </h2>
-        <h5 className="author">
-            by {subreddit[0].toUpperCase() + subreddit.slice(1)}&nbsp;
-            <a href={`https://www.reddit.com${permalink}`}>
-                <i class="fa-solid fa-up-right-from-square"></i>
-            </a>
-        </h5>
+        <div className="title">
+            <h2>
+                {title}
+            </h2>
+        </div>
+        <div className="author">
+            <h5>
+                by {subreddit[0].toUpperCase() + subreddit.slice(1)}&nbsp;
+                <a href={`https://www.reddit.com${permalink}`}>
+                    <i class="fa-solid fa-up-right-from-square"></i>
+                </a>
+            </h5>
+            </div>
         <div className="label">
-            {score}
+            <Tag text={"Litio"} color={"purple"}/>
+            <Tag text={"PV"} color={"gold"}/>
         </div>
         <div className="text">
             Texto 
