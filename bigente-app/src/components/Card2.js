e3384a015896a36197aa30028579b03c41997e3f
joaquinlzs <lazojoaquin.a@gmail.com>
2023-10-03 08:37:23 -0300

    add card size as function of the score

--- bigente-app/src/components/Card2.js
+++ bigente-app/src/components/Card2.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './CSS/Card2.css';
 import Tag from './Tag';
 import tiempoPasadoHastaHoy from '../functions/dateUnix';
+import getCardSize from '../functions/cardSize';
 
 function Card({ title, subreddit, selftext, score, thumbnail, permalink, tags, created }) {
     const [flipped, setFlipped] = useState(false);
@@ -13,18 +14,11 @@ function Card({ title, subreddit, selftext, score, thumbnail, permalink, tags, c
         }
     };
 
-    function getRandomCardSize() {
-        const cardSizes = ['card-small', 'card-medium', 'card-large'];
-        const randomIndex = Math.floor(Math.random() * cardSizes.length);
-        return cardSizes[randomIndex];
-    }
-    
-    // Ejemplo de uso:
-    const randomCardSize = getRandomCardSize();
+
 
     return (
     <div 
-        className={`card ${flipped ? 'flipped' : ''} ${getRandomCardSize()}`} 
+        className={`card ${flipped ? 'flipped' : ''} ${getCardSize(score)}`} 
         onClick={flipCard}
     >
         <div className="card-inner">
@@ -40,7 +34,8 @@ function Card({ title, subreddit, selftext, score, thumbnail, permalink, tags, c
                         by {subreddit[0].toUpperCase() + subreddit.slice(1)}&nbsp;
                         <a href={`https://www.reddit.com${permalink}`}>
                             <i class="fa-solid fa-up-right-from-square"></i>
-                        </a>
+                        </a>&nbsp;
+                        [{score}]
                     </h5>
                     </div>
                 <div className="label">
