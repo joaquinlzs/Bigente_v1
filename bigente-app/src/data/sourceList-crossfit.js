34dd7b8a011d4f1719b07b698a0c1a3414f6aa0f
joaquinlzs <lazojoaquin.a@gmail.com>
2023-10-11 08:07:53 -0300

    add sourcelists

---
+++ bigente-app/src/data/sourceList-crossfit.js
@@ -0,0 +1,22 @@
+const sourceList = {
+    3001: {
+        name: "Subreddit Crossfit",
+        type: "reddit",
+        link: "https://www.reddit.com/r/crossfit/",
+        comments: "Primer subreddit",
+    },
+
+    3002: {
+        name: "Subreddit Crossfit Hate",
+        type: "reddit",
+        link: "https://www.reddit.com/r/crossfithate/",
+        comments: "Segundo subreddit",
+    },
+
+    3003: {
+        name: "Subreddit Crossfit Hate",
+        type: "reddit",
+        link: "https://www.reddit.com/r/crossfitwod/",
+        comments: "Tercer subreddit"
+    },
+}
